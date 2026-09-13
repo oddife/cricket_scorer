9910dcbeb4327ab27f1f9a5318ccad143ad40393
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 21:57:50 +0530

    Add ball event repository contract

---
+++ lib/data/repositories/ball_event_repository.dart
@@ -0,0 +1,8 @@
+import '../../domain/scoring/models/ball_event.dart';
+
+abstract interface class BallEventRepository {
+  Future<BallEvent> create(BallEvent event);
+  Future<List<BallEvent>> getForInnings(int inningsId);
+  Future<BallEvent?> getBySequence(int inningsId, int sequenceNumber);
+  Future<void> deleteById(int id);
+}

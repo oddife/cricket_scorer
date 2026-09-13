3c9000a6d10b5c433fa529de431504dde1725b80
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 15:23:25 +0530

    Add tournament repository contract

---
+++ lib/data/repositories/tournament_repository.dart
@@ -0,0 +1,11 @@
+import '../../domain/tournaments/models/tournament.dart';
+
+abstract interface class TournamentRepository {
+  Future<List<Tournament>> getAll();
+
+  Future<Tournament> create(Tournament tournament);
+
+  Future<void> update(Tournament tournament);
+
+  Future<void> deactivate(int tournamentId);
+}

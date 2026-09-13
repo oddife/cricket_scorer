c8e4477f4f30e910777ffe06bfe0d7a4e71e165e
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 17:17:15 +0530

    Wire Match repository into database providers

--- lib/core/database/database_provider.dart
+++ lib/core/database/database_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../data/database/app_database.dart';
+import '../../data/repositories/drift_match_repository.dart';
 import '../../data/repositories/drift_player_repository.dart';
 import '../../data/repositories/drift_team_player_repository.dart';
 import '../../data/repositories/drift_team_repository.dart';
+import '../../data/repositories/match_repository.dart';
 import '../../data/repositories/player_repository.dart';
 import '../../data/repositories/team_player_repository.dart';
 import '../../data/repositories/team_repository.dart';
@@ -24,4 +26,8 @@ final teamRepositoryProvider = Provider<TeamRepository>((ref) {
 
 final teamPlayerRepositoryProvider = Provider<TeamPlayerRepository>((ref) {
   return DriftTeamPlayerRepository(ref.watch(appDatabaseProvider));
+});
+
+final matchRepositoryProvider = Provider<MatchRepository>((ref) {
+  return DriftMatchRepository(ref.watch(appDatabaseProvider));
 });

98270e08cfea92f1531c1b8b2150a8d55caeefb4
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 15:44:16 +0530

    Add teams and players to Drift database

--- lib/data/database/app_database.dart
+++ lib/data/database/app_database.dart
@@ -1,14 +1,17 @@
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 
+import 'tables/players.dart';
+import 'tables/team_players.dart';
+import 'tables/teams.dart';
 import 'tables/tournaments.dart';
 
 part 'app_database.g.dart';
 
-@DriftDatabase(tables: [Tournaments])
+@DriftDatabase(tables: [Players, Teams, TeamPlayers, Tournaments])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(driftDatabase(name: 'cricket_scorer'));
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
 }

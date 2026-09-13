34af4dace768fed7488505d75d184e4f96c90155
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 21:44:32 +0530

    Add innings state models

---
+++ lib/domain/innings/models/innings_state.dart
@@ -0,0 +1,70 @@
+import '../../scoring/models/ball_event.dart';
+
+class BatterInningsState {
+  const BatterInningsState({
+    required this.playerId,
+    required this.runs,
+    required this.balls,
+    required this.fours,
+    required this.sixes,
+    required this.isOut,
+  });
+
+  final int playerId;
+  final int runs;
+  final int balls;
+  final int fours;
+  final int sixes;
+  final bool isOut;
+}
+
+class BowlerInningsState {
+  const BowlerInningsState({
+    required this.playerId,
+    required this.legalBalls,
+    required this.runsConceded,
+    required this.wickets,
+  });
+
+  final int playerId;
+  final int legalBalls;
+  final int runsConceded;
+  final int wickets;
+}
+
+class InningsState {
+  const InningsState({
+    required this.score,
+    required this.wickets,
+    required this.legalBalls,
+    required this.ballsPerOver,
+    required this.strikerId,
+    required this.nonStrikerId,
+    required this.bowlerId,
+    required this.wides,
+    required this.noBalls,
+    required this.byes,
+    required this.legByes,
+    required this.batters,
+    required this.bowlers,
+    required this.ballCount,
+  });
+
+  final int score;
+  final int wickets;
+  final int legalBalls;
+  final int ballsPerOver;
+  final int strikerId;
+  final int nonStrikerId;
+  final int bowlerId;
+  final int wides;
+  final int noBalls;
+  final int byes;
+  final int legByes;
+  final Map<int, BatterInningsState> batters;
+  final Map<int, BowlerInningsState> bowlers;
+  final int ballCount;
+
+  int get completedOvers => legalBalls ~/ ballsPerOver;
+  int get legalBallsInCurrentOver => legalBalls % ballsPerOver;
+}

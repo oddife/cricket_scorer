14020a16488821463db53b29b74520c49e2f6abe
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 18:33:56 +0530

    Add innings Riverpod providers

---
+++ lib/features/matches/providers/innings_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/database/database_provider.dart';
+import '../../../domain/innings/models/innings.dart';
+
+final inningsByMatchProvider = FutureProvider.family<List<Innings>, int>(
+  (ref, matchId) => ref.watch(inningsRepositoryProvider).getForMatch(matchId),
+);
+
+final inningsByMatchAndNumberProvider =
+    FutureProvider.family<Innings?, ({int matchId, int inningsNumber})>(
+  (ref, args) => ref
+      .watch(inningsRepositoryProvider)
+      .getByMatchAndNumber(args.matchId, args.inningsNumber),
+);

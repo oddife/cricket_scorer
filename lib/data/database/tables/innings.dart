063425d99eae76315adbac3bb172d22ee807d4e5
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 18:31:42 +0530

    Add Drift innings table

---
+++ lib/data/database/tables/innings.dart
@@ -0,0 +1,23 @@
+import 'package:drift/drift.dart';
+
+class Innings extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get matchId => integer()();
+  IntColumn get inningsNumber => integer()();
+  IntColumn get battingTeamId => integer()();
+  IntColumn get bowlingTeamId => integer()();
+  IntColumn get openingStrikerId => integer()();
+  IntColumn get openingNonStrikerId => integer()();
+  IntColumn get openingBowlerId => integer()();
+  IntColumn get oversPerInnings => integer()();
+  IntColumn get ballsPerOver => integer()();
+  BoolColumn get twoBowlerMode => boolean()();
+  IntColumn get status => integer().withDefault(const Constant(0))();
+  DateTimeColumn get startedAt => dateTime().nullable()();
+  DateTimeColumn get completedAt => dateTime().nullable()();
+
+  @override
+  List<Set<Column>> get uniqueKeys => [
+        {matchId, inningsNumber},
+      ];
+}

f1b46f6668296645f36db001ea3935f135b4dd86
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 15:42:06 +0530

    Fix widget test SharedPreferences override

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:cricket_scorer/app/app.dart';
+import 'package:cricket_scorer/app/theme/theme_mode_provider.dart';
 
 void main() {
   testWidgets('Cricket Scorer app loads', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+    final preferences = await SharedPreferences.getInstance();
+
     await tester.pumpWidget(
-      const ProviderScope(
-        child: CricketScorerApp(),
+      ProviderScope(
+        overrides: [
+          sharedPreferencesProvider.overrideWithValue(preferences),
+        ],
+        child: const CricketScorerApp(),
       ),
     );
 

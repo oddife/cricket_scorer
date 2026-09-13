09ddf35d5428d253c4edb8fdfe74e1679d05caf3
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 15:45:32 +0530

    Add database and team/player repository providers

---
+++ lib/core/database/database_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../data/database/app_database.dart';
+import '../../data/repositories/drift_player_repository.dart';
+import '../../data/repositories/drift_team_repository.dart';
+import '../../data/repositories/player_repository.dart';
+import '../../data/repositories/team_repository.dart';
+
+final appDatabaseProvider = Provider<AppDatabase>((ref) {
+  final database = AppDatabase();
+  ref.onDispose(database.close);
+  return database;
+});
+
+final playerRepositoryProvider = Provider<PlayerRepository>((ref) {
+  return DriftPlayerRepository(ref.watch(appDatabaseProvider));
+});
+
+final teamRepositoryProvider = Provider<TeamRepository>((ref) {
+  return DriftTeamRepository(ref.watch(appDatabaseProvider));
+});

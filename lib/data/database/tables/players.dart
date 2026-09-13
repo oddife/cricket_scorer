2511cb10c066175c21d3990a3656b099dea9837a
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 15:53:32 +0530

    Store player jersey and handedness

--- lib/data/database/tables/players.dart
+++ lib/data/database/tables/players.dart
@@ -5,6 +5,9 @@ class Players extends Table {
   TextColumn get name => text()();
   TextColumn get displayName => text()();
   TextColumn get photoPath => text().nullable()();
+  IntColumn get jerseyNumber => integer().nullable()();
+  IntColumn get battingStyle => integer().withDefault(const Constant(0))();
+  IntColumn get bowlingStyle => integer().withDefault(const Constant(0))();
   BoolColumn get isActive => boolean().withDefault(const Constant(true))();
   DateTimeColumn get createdAt => dateTime()();
   DateTimeColumn get updatedAt => dateTime()();

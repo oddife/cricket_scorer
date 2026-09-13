bea3d618d703f35eda2e990fc412fda96a25529b
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 15:37:40 +0530

    Persist selected theme mode

--- lib/app/theme/theme_mode_provider.dart
+++ lib/app/theme/theme_mode_provider.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final themeModeProvider = NotifierProvider<ThemeModeNotifier, ThemeMode>(
   ThemeModeNotifier.new,
 );
 
 class ThemeModeNotifier extends Notifier<ThemeMode> {
+  static const _preferenceKey = 'theme_mode';
+
+  late final SharedPreferences _preferences;
+
   @override
-  ThemeMode build() => ThemeMode.system;
+  ThemeMode build() {
+    _preferences = ref.watch(sharedPreferencesProvider);
+    return _readMode();
+  }
 
   void setMode(ThemeMode mode) {
     state = mode;
+    _preferences.setString(_preferenceKey, mode.name);
+  }
+
+  ThemeMode _readMode() {
+    final value = _preferences.getString(_preferenceKey);
+    return ThemeMode.values.firstWhere(
+      (mode) => mode.name == value,
+      orElse: () => ThemeMode.system,
+    );
   }
 }
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError('SharedPreferences must be overridden in main()');
+});

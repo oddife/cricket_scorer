f6a6de1bcdd0b065c88372503c1e1ebb396afb4d
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 21:20:01 +0530

    Add balls per over to scoring context

--- lib/domain/scoring/models/scoring_context.dart
+++ lib/domain/scoring/models/scoring_context.dart
@@ -4,6 +4,7 @@ class ScoringContext {
     required this.sequenceNumber,
     required this.overNumber,
     required this.legalBallsInCurrentOver,
+    required this.ballsPerOver,
     required this.bowlerId,
     required this.strikerId,
     required this.nonStrikerId,
@@ -14,6 +15,7 @@ class ScoringContext {
   final int sequenceNumber;
   final int overNumber;
   final int legalBallsInCurrentOver;
+  final int ballsPerOver;
   final int bowlerId;
   final int strikerId;
   final int nonStrikerId;

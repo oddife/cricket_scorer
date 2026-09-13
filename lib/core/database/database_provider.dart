d49f89ddb18d088e1d5e187ab33a58dd64cd55b1
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 15:51:51 +0530

    Register team player repository

--- lib/core/database/database_provider.dart
+++ lib/core/database/database_provider.dart
@@ -2,8 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../data/database/app_database.dart';
 import '../../data/repositories/drift_player_repository.dart';
+import '../../data/repositories/drift_team_player_repository.dart';
 import '../../data/repositories/drift_team_repository.dart';
 import '../../data/repositories/player_repository.dart';
+import '../../data/repositories/team_player_repository.dart';
 import '../../data/repositories/team_repository.dart';
 
 final appDatabaseProvider = Provider<AppDatabase>((ref) {
@@ -18,4 +20,8 @@ final playerRepositoryProvider = Provider<PlayerRepository>((ref) {
 
 final teamRepositoryProvider = Provider<TeamRepository>((ref) {
   return DriftTeamRepository(ref.watch(appDatabaseProvider));
+});
+
+final teamPlayerRepositoryProvider = Provider<TeamPlayerRepository>((ref) {
+  return DriftTeamPlayerRepository(ref.watch(appDatabaseProvider));
 });

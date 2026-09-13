9edcc27bd12dad34ef52ae8df71fda16833c4cb4
oddife <33018182+oddife@users.noreply.github.com>
2026-09-13 17:54:33 +0530

    Add team player repository provider

--- lib/features/teams/providers/team_player_provider.dart
+++ lib/features/teams/providers/team_player_provider.dart
@@ -2,7 +2,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/database_provider.dart';
 import '../../../domain/players/models/player.dart';
+import '../../../domain/teams/models/team_player.dart';
 
 final teamPlayersProvider = FutureProvider.family<List<Player>, int>((ref, teamId) {
   return ref.watch(teamPlayerRepositoryProvider).getPlayersForTeam(teamId);
+});
+
+final teamPlayerRepositoryProvider = Provider((ref) {
+  return ref.watch(appDatabaseProvider).teamPlayerRepository;
 });

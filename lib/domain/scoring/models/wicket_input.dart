173c29725ffdc7ee0150473e8cfd6fe3ee2a3b8e
oddife <33018182+oddife@users.noreply.github.com>
2026-09-14 19:44:13 +0530

    Add wicket workflow input model

---
+++ lib/domain/scoring/models/wicket_input.dart
@@ -0,0 +1,22 @@
+import '../enums/run_out_end.dart';
+import '../enums/wicket_type.dart';
+
+/// Scorer input used to describe a wicket before it becomes part of a
+/// persisted BallEvent.
+class WicketInput {
+  const WicketInput({
+    required this.type,
+    required this.dismissedPlayerId,
+    this.fielderId,
+    this.runOutEnd,
+    this.completedRuns = 0,
+    this.crossedBeforeWicket = false,
+  });
+
+  final WicketType type;
+  final int dismissedPlayerId;
+  final int? fielderId;
+  final RunOutEnd? runOutEnd;
+  final int completedRuns;
+  final bool crossedBeforeWicket;
+}

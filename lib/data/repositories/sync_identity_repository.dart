ff34f54a742c758d9bc5868ba4aa345331d41143
oddife <33018182+oddife@users.noreply.github.com>
2026-09-15 23:25:05 +0530

    Extend stable sync identity repository to BallEvents

--- lib/data/repositories/sync_identity_repository.dart
+++ lib/data/repositories/sync_identity_repository.dart
@@ -1,6 +1,8 @@
 abstract interface class SyncIdentityRepository {
   Future<String> ensureMatchSyncId(int matchId);
   Future<String> ensureInningsSyncId(int inningsId);
+  Future<String> ensureBallEventSyncId(int ballEventId);
   Future<String?> getMatchSyncId(int matchId);
   Future<String?> getInningsSyncId(int inningsId);
+  Future<String?> getBallEventSyncId(int ballEventId);
 }

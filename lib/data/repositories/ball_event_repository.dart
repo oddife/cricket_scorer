2bc58baeb5180cef45f85fee7c1f4e5b7d455e2e
oddife <33018182+oddife@users.noreply.github.com>
2026-09-15 14:41:03 +0530

    Add persisted wicket replacement update workflow

--- lib/data/repositories/ball_event_repository.dart
+++ lib/data/repositories/ball_event_repository.dart
@@ -4,5 +4,6 @@ abstract interface class BallEventRepository {
   Future<BallEvent> create(BallEvent event);
   Future<List<BallEvent>> getForInnings(int inningsId);
   Future<BallEvent?> getBySequence(int inningsId, int sequenceNumber);
+  Future<void> updateWicketReplacement({required int ballEventId, required int replacementBatterId});
   Future<void> deleteById(int id);
 }

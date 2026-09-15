6895170311678f14a68a11dbf1004328a78e60bb
oddife <33018182+oddife@users.noreply.github.com>
2026-09-15 23:25:18 +0530

    Add stable BallEvent sync identity

--- lib/data/repositories/drift_sync_identity_repository.dart
+++ lib/data/repositories/drift_sync_identity_repository.dart
@@ -12,20 +12,26 @@ class DriftSyncIdentityRepository implements SyncIdentityRepository {
   final Random _random = Random.secure();
 
   @override
-  Future<String> ensureMatchSyncId(int matchId) =>
-      _ensure('match', matchId);
+  Future<String> ensureMatchSyncId(int matchId) => _ensure('match', matchId);
 
   @override
   Future<String> ensureInningsSyncId(int inningsId) =>
       _ensure('innings', inningsId);
 
   @override
-  Future<String?> getMatchSyncId(int matchId) =>
-      _get('match', matchId);
+  Future<String> ensureBallEventSyncId(int ballEventId) =>
+      _ensure('ball', ballEventId);
+
+  @override
+  Future<String?> getMatchSyncId(int matchId) => _get('match', matchId);
 
   @override
   Future<String?> getInningsSyncId(int inningsId) =>
       _get('innings', inningsId);
+
+  @override
+  Future<String?> getBallEventSyncId(int ballEventId) =>
+      _get('ball', ballEventId);
 
   Future<String> _ensure(String entityType, int localId) async {
     final existing = await _get(entityType, localId);
@@ -66,7 +72,9 @@ class DriftSyncIdentityRepository implements SyncIdentityRepository {
     final bytes = List<int>.generate(16, (_) => _random.nextInt(256));
     bytes[6] = (bytes[6] & 0x0f) | 0x40;
     bytes[8] = (bytes[8] & 0x3f) | 0x80;
-    final hex = bytes.map((value) => value.toRadixString(16).padLeft(2, '0')).join();
+    final hex = bytes
+        .map((value) => value.toRadixString(16).padLeft(2, '0'))
+        .join();
     return '${hex.substring(0, 8)}-${hex.substring(8, 12)}-'
         '${hex.substring(12, 16)}-${hex.substring(16, 20)}-'
         '${hex.substring(20)}';

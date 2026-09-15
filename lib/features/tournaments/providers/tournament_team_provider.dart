3fa112168e97f3370538588d714ad1b0d7c6a60a
oddife <33018182+oddife@users.noreply.github.com>
2026-09-15 18:42:12 +0530

    Fix tournament team Riverpod notifier

--- lib/features/tournaments/providers/tournament_team_provider.dart
+++ lib/features/tournaments/providers/tournament_team_provider.dart
@@ -3,24 +3,26 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/database/database_provider.dart';
 import '../../../domain/teams/models/team.dart';
 
-final tournamentTeamsProvider =
-    FutureProvider.family<List<Team>, int>((ref, tournamentId) {
-  return ref.watch(tournamentTeamRepositoryProvider).getTeams(tournamentId);
-});
+final tournamentTeamsProvider = FutureProvider.family<List<Team>, int>(
+  (ref, tournamentId) =>
+      ref.watch(tournamentTeamRepositoryProvider).getTeams(tournamentId),
+);
 
-class TournamentTeamNotifier extends FamilyAsyncNotifier<List<Team>, int> {
+final tournamentTeamNotifierProvider =
+    NotifierProvider.family<TournamentTeamNotifier, void, int>(
+  TournamentTeamNotifier.new,
+);
+
+class TournamentTeamNotifier extends FamilyNotifier<void, int> {
   @override
-  Future<List<Team>> build(int tournamentId) {
-    return ref.watch(tournamentTeamRepositoryProvider).getTeams(tournamentId);
-  }
+  void build(int tournamentId) {}
 
   Future<void> addTeam(int teamId) async {
     await ref.read(tournamentTeamRepositoryProvider).addTeam(
           tournamentId: arg,
           teamId: teamId,
         );
-    ref.invalidateSelf();
-    await future;
+    ref.invalidate(tournamentTeamsProvider(arg));
   }
 
   Future<void> removeTeam(int teamId) async {
@@ -28,12 +30,6 @@ class TournamentTeamNotifier extends FamilyAsyncNotifier<List<Team>, int> {
           tournamentId: arg,
           teamId: teamId,
         );
-    ref.invalidateSelf();
-    await future;
+    ref.invalidate(tournamentTeamsProvider(arg));
   }
 }
-
-final tournamentTeamNotifierProvider =
-    AsyncNotifierProvider.family<TournamentTeamNotifier, List<Team>, int>(
-  TournamentTeamNotifier.new,
-);

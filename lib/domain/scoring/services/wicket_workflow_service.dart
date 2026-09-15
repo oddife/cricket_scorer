1ced0b912f8d11a8decc5ba76bd4edfc34c961ff
oddife <33018182+oddife@users.noreply.github.com>
2026-09-15 15:16:10 +0530

    Fix wicket workflow validation for combined deliveries

--- lib/domain/scoring/services/wicket_workflow_service.dart
+++ lib/domain/scoring/services/wicket_workflow_service.dart
@@ -21,7 +21,6 @@ class WicketWorkflowService {
     if (strikerId <= 0 || nonStrikerId <= 0 || strikerId == nonStrikerId) throw ArgumentError('A valid striker and non-striker are required.');
     if (input.completedRuns < 0) throw ArgumentError.value(input.completedRuns, 'completedRuns');
     if (input.replacementBatterId != null && input.replacementBatterId! <= 0) throw ArgumentError.value(input.replacementBatterId, 'replacementBatterId');
-    _validateDeliveryRuns(input, deliveryType);
 
     final dismissedIsBatter = input.dismissedPlayerId == strikerId || input.dismissedPlayerId == nonStrikerId;
     if (!dismissedIsBatter) throw ArgumentError('The dismissed player must be the striker or non-striker.');
@@ -55,23 +54,5 @@ class WicketWorkflowService {
     if (deliveryType == DeliveryType.noBall && input.type != WicketType.runOut && input.type != WicketType.obstructingField) throw ArgumentError('On a no-ball, only run out or obstructing the field can be recorded here.');
     if (deliveryType == DeliveryType.wide && input.type != WicketType.stumped && input.type != WicketType.runOut && input.type != WicketType.hitWicket && input.type != WicketType.obstructingField) throw ArgumentError('On a wide, only hit wicket, obstructing the field, run out or stumped can be recorded here.');
     if ((deliveryType == DeliveryType.bye || deliveryType == DeliveryType.legBye) && input.type != WicketType.stumped && input.type != WicketType.runOut && input.type != WicketType.hitWicket && input.type != WicketType.obstructingField) throw ArgumentError('On a bye or leg-bye, only hit wicket, obstructing the field, run out or stumped can be recorded here.');
-  }
-
-  void _validateDeliveryRuns(WicketInput input, DeliveryType deliveryType) {
-    for (final entry in <String, int>{'batterRuns': input.batterRuns, 'byeRuns': input.byeRuns, 'legByeRuns': input.legByeRuns, 'wideRuns': input.wideRuns, 'noBallRuns': input.noBallRuns}.entries) {
-      if (entry.value < 0) throw ArgumentError.value(entry.value, entry.key);
-    }
-    switch (deliveryType) {
-      case DeliveryType.normal:
-        if (input.byeRuns != 0 || input.legByeRuns != 0 || input.wideRuns != 0 || input.noBallRuns != 0) throw ArgumentError('Normal wicket delivery cannot contain extras.');
-      case DeliveryType.wide:
-        if (input.wideRuns < 1 || input.batterRuns != 0 || input.byeRuns != 0 || input.legByeRuns != 0 || input.noBallRuns != 0) throw ArgumentError('Wide wicket delivery must contain wide runs only.');
-      case DeliveryType.noBall:
-        if (input.noBallRuns != 1 || input.wideRuns != 0 || (input.byeRuns != 0 && input.legByeRuns != 0)) throw ArgumentError('No-ball wicket delivery has invalid extras.');
-      case DeliveryType.bye:
-        if (input.byeRuns < 1 || input.batterRuns != 0 || input.legByeRuns != 0 || input.wideRuns != 0 || input.noBallRuns != 0) throw ArgumentError('Bye wicket delivery must contain bye runs only.');
-      case DeliveryType.legBye:
-        if (input.legByeRuns < 1 || input.batterRuns != 0 || input.byeRuns != 0 || input.wideRuns != 0 || input.noBallRuns != 0) throw ArgumentError('Leg-bye wicket delivery must contain leg-bye runs only.');
-    }
   }
 }

a7d3e1152ba2fa6d157203de81e066e3bced5af4
oddife <33018182+oddife@users.noreply.github.com>
2026-09-16 16:14:12 +0530

    Expose all memberships for synchronization

--- lib/data/repositories/team_player_repository.dart
+++ lib/data/repositories/team_player_repository.dart
@@ -4,11 +4,8 @@ import '../../domain/teams/models/team_player.dart';
 abstract interface class TeamPlayerRepository {
   Future<List<Player>> getPlayersForTeam(int teamId);
   Future<List<TeamPlayer>> getActiveMemberships();
-  Future<TeamPlayer> addPlayerToTeam({
-    required int teamId,
-    required int playerId,
-    int? jerseyNumber,
-  });
+  Future<List<TeamPlayer>> getAllMemberships();
+  Future<TeamPlayer> addPlayerToTeam({required int teamId, required int playerId, int? jerseyNumber});
   Future<void> removePlayerFromTeam(int teamId, int playerId);
   Future<void> updateJerseyNumber(int teamId, int playerId, int? jerseyNumber);
 }

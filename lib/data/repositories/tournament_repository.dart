4f2181275901f8eb9bdf073b4d982ddb71edab61
oddife <33018182+oddife@users.noreply.github.com>
2026-09-16 16:14:06 +0530

    Expose inactive tournaments for synchronization

--- lib/data/repositories/tournament_repository.dart
+++ lib/data/repositories/tournament_repository.dart
@@ -2,10 +2,8 @@ import '../../domain/tournaments/models/tournament.dart';
 
 abstract interface class TournamentRepository {
   Future<List<Tournament>> getAll();
-
+  Future<List<Tournament>> getAllIncludingInactive();
   Future<Tournament> create(Tournament tournament);
-
   Future<void> update(Tournament tournament);
-
   Future<void> deactivate(int tournamentId);
 }

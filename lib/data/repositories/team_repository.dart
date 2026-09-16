eabcd1b604e5aecf174f4d0eddcecd4eb30e9219
oddife <33018182+oddife@users.noreply.github.com>
2026-09-16 15:44:24 +0530

    sync: expose inactive teams for recovery

--- lib/data/repositories/team_repository.dart
+++ lib/data/repositories/team_repository.dart
@@ -2,6 +2,7 @@ import '../../domain/teams/models/team.dart';
 
 abstract interface class TeamRepository {
   Future<List<Team>> getAll();
+  Future<List<Team>> getAllIncludingInactive();
   Future<Team> create(Team team);
   Future<void> update(Team team);
   Future<void> deactivate(int teamId);

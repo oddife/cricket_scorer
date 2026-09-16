11eb0ec04a04b0a6250d8e5fbbdfea5e950db596
oddife <33018182+oddife@users.noreply.github.com>
2026-09-16 14:02:29 +0530

    Align standings provider with service dependencies

--- lib/features/tournaments/providers/tournament_standings_provider.dart
+++ lib/features/tournaments/providers/tournament_standings_provider.dart
@@ -2,19 +2,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/database_provider.dart';
 import '../../../domain/tournaments/models/tournament_standing.dart';
+import '../../matches/providers/match_provider.dart';
 import '../services/tournament_standings_service.dart';
 import 'tournament_provider.dart';
-import '../../matches/providers/match_provider.dart';
+import 'tournament_team_provider.dart';
 
 final tournamentStandingsProvider = FutureProvider.family<List<TournamentStanding>, int>(
   (ref, tournamentId) async {
     ref.watch(matchProvider);
     ref.watch(tournamentPointsRulesProvider(tournamentId));
+    ref.watch(tournamentTeamsProvider(tournamentId));
     final service = TournamentStandingsService(
       matchRepository: ref.watch(matchRepositoryProvider),
       inningsRepository: ref.watch(inningsRepositoryProvider),
       ballEventRepository: ref.watch(ballEventRepositoryProvider),
-      teamRepository: ref.watch(teamRepositoryProvider),
       tournamentTeamRepository: ref.watch(tournamentTeamRepositoryProvider),
       pointsRepository: ref.watch(tournamentPointsRepositoryProvider),
     );

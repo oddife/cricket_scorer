c0dc6dbbc6cbbcb60a37b2d2bec22bcabec24d8a
oddife <33018182+oddife@users.noreply.github.com>
2026-09-16 16:12:06 +0530

    Add inactive player retrieval for sync

--- lib/data/repositories/player_repository.dart
+++ lib/data/repositories/player_repository.dart
@@ -2,6 +2,7 @@ import '../../domain/players/models/player.dart';
 
 abstract interface class PlayerRepository {
   Future<List<Player>> getAll();
+  Future<List<Player>> getAllIncludingInactive();
   Future<Player> create(Player player);
   Future<void> update(Player player);
   Future<void> deactivate(int playerId);
